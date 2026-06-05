4d46521ecfac1a5eb6283a03ad88656e30c80d0a
Alexey Maksimov <domenoferta@gmail.com>
2026-06-06 01:06:48 +0300

    урок 8 вкл. редактирование

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -7,6 +7,7 @@ app_name = 'birthday'
 
 urlpatterns = [
     path('', views.birthday, name='create'),
-    # Новый маршрут.
-    path('list/', views.birthday_list, name='list')
+    # Новые маршруты.
+    path('list/', views.birthday_list, name='list'),
+    path('<int:pk>/edit/', views.birthday, name='edit'),
 ]
